99583c47b77eac880fb4cc21f73d448ce31e871d
sicheng.lai <956006031@qq.com>
2018-08-14 11:07:04 +0800

    format code

--- driver/uart/uart.h
+++ driver/uart/uart.h
@@ -1,11 +1,11 @@
 #ifndef __USART_H
 #define __USART_H
-#include "stdio.h"	
-#include "sys.h" 
-//////////////////////////////////////////////////////////////////////////////////	 
+#include "stdio.h"
+#include "sys.h"
+//////////////////////////////////////////////////////////////////////////////////
 //本程序只供学习使用，未经作者许可，不得用于其它任何用途
 //ALIENTEK STM32开发板
-//串口1初始化		   
+//串口1初始化
 //正点原子@ALIENTEK
 //技术论坛:www.openedv.com
 //修改日期:2012/8/18
@@ -14,7 +14,7 @@
 //Copyright(C) 广州市星翼电子科技有限公司 2009-2019
 //All rights reserved
 //********************************************************************************
-//V1.3修改说明 
+//V1.3修改说明
 //支持适应不同频率下的串口波特率设置.
 //加入了对printf的支持
 //增加了串口接收命令功能.
@@ -28,9 +28,9 @@
 //1,增加了对UCOSII的支持
 #define USART_REC_LEN  			200  	//定义最大接收字节数 200
 #define EN_USART1_RX 			1		//使能（1）/禁止（0）串口1接收
-	  	
-extern u8  USART_RX_BUF[USART_REC_LEN]; //接收缓冲,最大USART_REC_LEN个字节.末字节为换行符 
-extern u16 USART_RX_STA;         		//接收状态标记	
+
+extern u8  USART_RX_BUF[USART_REC_LEN]; //接收缓冲,最大USART_REC_LEN个字节.末字节为换行符
+extern u16 USART_RX_STA;         		//接收状态标记
 //如果想串口中断接收，请不要注释以下宏定义
 void uart_init(u32 bound);
 #endif
